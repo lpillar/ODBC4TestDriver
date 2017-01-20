8d9a48cdc4a832b246ad181b0f9f8e7d0957e28c
lpillar <lukepillar@gmail.com>
2017-01-20 14:26:03 -0800

    Revert "ODBC Driver Skeleton"
    This reverts commit 4c1f11b15f8031bda068919df242a4a685ced98e.

--- ODBC4TestDriver/DllMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "windows.h"
-//#include "sqlext.h"
-#include "odbcinst.h"
-
-BOOL WINAPI DLLMain(HINSTANCE dll, DWORD reason, LPVOID reserved)
-{
-    MessageBox(GetDesktopWindow(), TEXT("Not Implemented"), NULL, MB_OK);
-    return TRUE;
-}
